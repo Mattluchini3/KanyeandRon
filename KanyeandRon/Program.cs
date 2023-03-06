c61f08bcaf504f5d436897fcb6418f8fce15a65f
Matthew Luchini <luch@Matthews-MBP.lan>
2023-03-06 12:09:03 -0500

    Kanye/Ron

---
+++ KanyeandRon/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+
+namespace KanyeandRon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Quote.KanyeQuote();
+
+            Quote.RonQuote();
+        }
+    }
+}
+
+
+
